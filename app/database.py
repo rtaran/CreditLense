ff6a7aa4c4dd4ed8a81f127bc35865e84e52d83a
kurtis07-AI <kurtisk2@gmail.com>
2025-04-15 19:46:05 +0300

    updated 1: clean-up

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./financial_data.db"
+DATABASE_URL = "sqlite:///./database/financial_data.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
--- main.py
+++
@@ -1,9 +0,0 @@
-from uv import Uv
-from db.models import Base
-from db.database import engine
-from api import document
-
-app = Uv()
-Base.metadata.create_all(bind=engine)
-
-app.include_router(document.router, prefix="/document")
