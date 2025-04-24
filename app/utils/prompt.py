1e8a8114013f9ef6fa203a034bbabff0443f5b65
phlasse <zastrow.phillip@gmail.com>
2025-04-24 10:39:21 +0200

    chore: food for thoughts

---
+++ app/utils/prompt.py
@@ -0,0 +1,7 @@
+DOCUMENTS_SYSTEM_PROMPT = f"""
+you will receive a pdf in string and have to extract the following information:
+columns...
+
+return you findings in the strict json format provided
+
+"""
